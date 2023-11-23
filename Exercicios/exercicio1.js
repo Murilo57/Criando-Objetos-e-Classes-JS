5ee64527644c53dccd767dbfa5d96f9ce6bb61cf
Murilo <murilo.ti32@outlook.com>
2023-11-23 16:37:09 -0300

    ex1 inicio

---
+++ Exercicios/exercicio1.js
@@ -0,0 +1,13 @@
+/*
+    1 - Crie uma classe para representar carros.
+    Os carros possuem uma marca, uma cor e um gasto médio de combustivel por Kilômetro rodado.
+    Crie um método que dado a quantidade de quilómetros e o preço do combustivel gasto em reais para realizar este percurso
+ */
+
+class Carro {
+    marca;
+    cor;
+
+}
+
+var teste = input('teste:');
