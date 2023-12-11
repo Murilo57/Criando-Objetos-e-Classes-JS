b1d5d0956deb31f9c1ca8dfa8d2129c32ecb5dd8
Murilo <murilo.ti32@outlook.com>
2023-12-11 14:49:25 -0300

    Exercicio1

--- Exercicios/exercicio1.js
+++ Exercicios/exercicio1.js
@@ -4,10 +4,23 @@
     Crie um método que dado a quantidade de quilómetros e o preço do combustivel gasto em reais para realizar este percurso
  */
 
-class Carro {
-    marca;
-    cor;
+class Carro {    
+    constructor(marca, cor, distanciaKM, litrosTanque) {
+        this.marca = marca;
+        this.cor = cor;   
+        this.distanciaKM = distanciaKM;
+        this.litrosTanque = litrosTanque; 
+        this.gastoMedioKM();
+    }
+
+    
+    gastoMedioKM() {
+        let consumoMedio = (this.distanciaKM / this.litrosTanque)
+        console.log(`O seu carro da marca ${this.marca} da cor ${this.cor} tem o consumo médio de: ${consumoMedio}`)
+    }
 
 }
 
-var teste = input('teste:');
+const Toyota = new Carro('Toyota','vermelho', 320, 25)
+
+console.log(Toyota)
